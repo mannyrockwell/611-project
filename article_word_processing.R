67779633036e1a018259baa7cd9475d220cc0838
mannyrockwell <erockwell@unc.edu>
2021-12-01 15:14:47 -0500

    updated word processing visualizations

--- article_word_processing.R
+++ article_word_processing.R
@@ -16,10 +16,11 @@ library(viridis)
 library(colorRamps)
 library(webshot)
 library(htmlwidgets)
+library(cowplot)
 webshot::install_phantomjs()
 
 pitchfork = read.csv("derived_data/pitchfork_clean.csv", header = TRUE)
-pitchfork_bad = as_tibble(pitchfork) %>% filter(score <= 4.7)
+pitchfork_bad = as_tibble(pitchfork) %>% filter(score <= 5.0)
 pitchfork = as_tibble(pitchfork) %>% filter(bnm == 1)
 
 article_source <- pitchfork %>% select(review) %>% as.vector() %>% 
@@ -80,43 +81,87 @@ names(word_freq_nostop) <- c("word","frequency")
 
 freq_plot2 <- ggplot(data = word_freq[1:20,], aes(x = frequency, y = reorder(word, frequency))) +
   geom_bar(stat = "identity", fill = "seagreen") +
-  labs(title = "Most Frequenct Words: Good Reviews Removing Common Music Words", x = "Frequency", y ="Word") +
+  labs(title = "Most Frequent Words Good Reviews\n Removing Common Music Words", x = "Frequency", y ="Word") +
+  geom_text(aes(label = frequency), hjust = 1.1) +
   theme_bw()
 
 freq_plot1 <- ggplot(data = word_freq_nostop[1:20,], aes(x = frequency, y = reorder(word, frequency))) +
-  geom_bar(stat = "identity", fill = "firebrick") +
-  labs(title = "Most Frequenct Words: Good Reviews", x = "Frequency", y ="Word") +
+  geom_bar(stat = "identity", fill = "slateblue") +
+  labs(title = "Most Frequent Words: Good Reviews\n Only Removing Common Stop Words", x = "Frequency", y ="Word") +
+  geom_text(aes(label = frequency), hjust = 1.1) +
   theme_bw()
 
 freq_plot3 <- ggplot(data = word_freq_bad[1:20,], aes(x = frequency, y = reorder(word, frequency))) +
-  geom_bar(stat = "identity", fill = "slateblue") +
-  labs(title = "Most Frequenct Words: Bad Reviews", x = "Frequency", y ="Word") +
+  geom_bar(stat = "identity", fill = "firebrick") +
+  labs(title = "Most Frequent Words: Bad Reviews\n Removing Common Music Words", x = "Frequency", y ="Word") +
+  geom_text(aes(label = frequency), hjust = 1.1) +
   theme_bw()
 
 freq_plot <- plot_grid(freq_plot1, freq_plot2, freq_plot3, ncol=3)
 freq_plot
 
+top_x <- 20
+
+word_freq_good_rank <- word_freq %>% mutate(rank = row_number(desc(frequency))) %>%
+  mutate(position = -1) %>%  filter(rank <= top_x)
+word_freq_bad_rank <- word_freq_bad %>% mutate(rank = row_number(desc(frequency))) %>%
+  mutate(position = 1) %>%  filter(rank <= top_x)
+
+line_data_good <- word_freq_good_rank %>% left_join(word_freq_bad_rank, by="word")
+line_data_bad <- word_freq_bad_rank %>% left_join(word_freq_good_rank, by="word")
+
+line_data <- rbind(line_data_good, line_data_bad) %>% filter(rank)distinct_at(vars(word)) %>%
+  mutate(good_rank=replace_na(rank.x,top_x+1),
+         bad_rank=replace_na(rank.y,top_x+1))
+
+word_freq_ranks <- union_all(word_freq_good_rank, word_freq_bad_rank)
+
+word_comparison <- ggplot(word_freq_ranks) +
+  geom_rect(aes(xmin=position-0.5, xmax=position+0.5, ymin=rank-0.45, ymax=rank+0.45, fill=rank), show.legend = FALSE) +
+  geom_label(aes(x=position, y=rank, label=word), color = "black") +
+  geom_segment(data=line_data[0:20,],aes(x=-0.5,xend=0.5, y=good_rank, yend=bad_rank, color = rank.x), show.legend = FALSE, size =1) +
+  scale_y_reverse(breaks = 1:top_x) +
+  scale_x_continuous(breaks=c(-1,1), labels=c("Good","Bad")) +
+  scale_color_viridis(name = 'distortion', option = "C", direction = 1) +
+  scale_fill_viridis(name = 'distortion', option = "C", direction = 1) +
+  labs(x="Quality of Album", y="Rank", title="How do Good Reviews Differ from Bad Reviews?");
+
+
+
+
+
 word_cloud2 <- wordcloud2(data=word_freq)
 word_cloud2
 
-word_cloud_bad <- wordcloud2(data=word_freq_bad)
+word_cloud_bad <- wordcloud2(data=word_freq_bad, size = 0.8)
 word_cloud_bad
 
 saveWidget(word_cloud2,"figures/wordcloud.html",selfcontained = F)
-webshot::webshot("figures/wordcloud.html","figures/wordcloud.png",vwidth = 1992, vheight = 1744, delay =10)
+webshot::webshot("figures/wordcloud.html","figures/wordcloud.png",vwidth = 1700, vheight = 1300, delay =20)
 
 saveWidget(word_cloud_bad,"figures/wordcloud_bad.html",selfcontained = F)
-webshot::webshot("figures/wordcloud_bad.html","figures/wordcloud_bad.png",vwidth = 1992, vheight = 1744, delay =10)
+webshot::webshot("figures/wordcloud_bad.html","figures/wordcloud_bad.png",vwidth = 1700, vheight = 1300, delay =20)
 
 if (!dir.exists("figures")){
   dir.create("figures")
   ggsave("figures/word_distribution_plot.png", 
-         width = 15, height = 5, 
+         width = 12, height = 7, 
          plot = freq_plot)
 } else {
   ggsave("figures/word_distribution_plot.png", 
-         width = 12, height = 5, 
+         width = 12, height = 7, 
          plot = freq_plot)
+}
+
+if (!dir.exists("figures")){
+  dir.create("figures")
+  ggsave("figures/word_comparison.png", 
+         width = 10, height = 7, 
+         plot = word_comparison)
+} else {
+  ggsave("figures/word_comparison.png", 
+         width = 10, height = 7, 
+         plot = word_comparison)
 }
 
 # colfunc <- colorRampPalette(c("black", "red"))
